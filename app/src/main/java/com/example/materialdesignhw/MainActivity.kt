f393e2ce84f412bc4b3d23d373b2948dc40cd7af
bahu4 <bahu4@mail.ru>
2020-11-09 02:13:10 +0300

    refactor

--- app/src/main/java/com/example/materialdesignhw/MainActivity.kt
+++ app/src/main/java/com/example/materialdesignhw/MainActivity.kt
@@ -7,18 +7,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-//        android:textColorHint="@color/colorPrimaryDark"
-//        app:boxBackgroundColor="@color/colorAccent"
-//        app:hintTextColor="@color/red"
-//        app:boxStrokeColor="@color/red"
-//        app:boxStrokeErrorColor="@color/yellow"
-//        app:boxStrokeWidth="5dp"
-//        app:boxStrokeWidthFocused="5dp"
-//        app:helperTextEnabled="true"
-//        app:helperText="@string/app_name"
-//        app:helperTextTextColor="@color/blue"
-//        app:counterTextColor="@color/colorPrimaryDark"
-//        app:counterOverflowTextColor="@color/blue"
     }
 }
