dac4eeb4a73384a80fc5fc0ccae93584596b0092
bahu4 <bahu4@mail.ru>
2020-11-28 19:45:24 +0300

    add TabActivity.kt

--- app/src/main/java/com/example/materialdesignhw/MainActivity.kt
+++ app/src/main/java/com/example/materialdesignhw/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.materialdesignhw
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.fragment_start.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        enterBtn.setOnClickListener {
+            val i = Intent(this,TabActivity::class.java)
+            startActivity(i)
+        }
     }
 }
