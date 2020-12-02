24afbc46d07cbde0fab17d1f3258ae9e2e6777b2
bahu4 <bahu4@mail.ru>
2020-12-02 20:17:47 +0300

    add LoginActivity.kt

---
+++ app/src/main/java/com/example/materialdesignhw/style_pack/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.example.materialdesignhw.style_pack.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
